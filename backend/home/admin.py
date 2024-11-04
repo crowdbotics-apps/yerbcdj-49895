fe12d63fc67824e20758f4d6e7c28f8cd91ccf01
crowdbotics-apps <team@crowdbotics.com>
2024-11-04 08:37:35 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Jcvbdnd
+
+admin.site.register(Jcvbdnd)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Jcvbdnd(models.Model):
+    "Generated Model"
+    teye = models.BigIntegerField()
+    vcbdjs = models.BigIntegerField()
